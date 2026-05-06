d39cac70b42564cac86836c55a27c206b4b6cb0b
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-06 16:47:43 +0200

    feat: redesign footer and implement NavButtons
    Fix footer to the bottom with MIT license year and centered layout.
    Implement NavButtons using ButtonGroup with checker/generator toggle.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import { ModeToggle } from './mode-toggle'
 
 export default function Footer() {
-  // const year = new Date().getFullYear()
+  const year = new Date().getFullYear()
 
   return (
-    <footer className="mt-20 px-4 pb-14 pt-10">
-      <ModeToggle />
+    <footer className="fixed bottom-0 w-full p-5 flex flex-row">
+      <div className="mr-auto">
+        <ModeToggle />
+      </div>
+      <span className=" flex justify-center items-center text-secondary-foreground">
+        MIT License © {year} Haze
+      </span>
+      <div className="ml-auto" /> {/* spacer */}
     </footer>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,1 +1,26 @@
-export const NavButtons = () => {}
+import { Button } from './ui/button'
+import { ButtonGroup, ButtonGroupSeparator } from './ui/button-group'
+
+// NavButtons.tsx
+type Site = 'check' | 'gen'
+
+type Props = {
+  site: Site
+  setSite: (site: Site) => void
+}
+
+export const NavButtons = ({ setSite }: Props) => {
+  return (
+    <div className="flex justify-center">
+      <ButtonGroup>
+        <Button className="cursor-pointer" onClick={() => setSite('check')}>
+          checker
+        </Button>
+        <ButtonGroupSeparator className="bg-accent" />
+        <Button className="cursor-pointer" onClick={() => setSite('gen')}>
+          generator
+        </Button>
+      </ButtonGroup>
+    </div>
+  )
+}
